01634144bc9ad21f0d7b69c657b5fa39a4ea65b7
nurs-dev <nurislamkadyrov01.gmail.com>
2024-11-20 09:42:46 +0600

    create a new project

---
+++ app.js
@@ -0,0 +1,52 @@
+
+// переменный let var columnRulyStyle
+// var ESS
+//lit const ES6 (EsmaScript6)
+
+// const myName=' Iliaz'
+// let myAge=12;
+
+// let messege='Hello' +myName+' '+myAge+' Wellcome to JS' //ESS
+// let newMassage=Hello ${myName} ${myAge} Wellcome to JS //ESS
+// console.log(messege);
+// console.log(newMassage);
+
+ const userName=prompt('What is your name')
+ const isMerried=confirm('Are you merried')
+ console.log(userName);
+ 
+ alert=('userName')
+ 
+// const num=parseInt(prompt('first san'));
+// const num2=parseInt(prompt('Second number'));
+// const summa=num + num2
+// console.log(summa);
+// const minus=num - num2
+// console.log(num+'-'+num2+'='+minus);
+// const umnoj=num * num2
+// console.log(umnoj);
+// const razdeli=num / num2
+// console.log(razdeli);
+
+
+const anime=['Blue lock', 'Onepachman', 'Naruto', 'DanDadan']
+
+for (i=0; i<anime.length; i++){
+    console.log('Bugunku korchu anime', anime[i]);
+}
+
+
+// const sandar=[]
+// for (i=0; i<3; i++){
+//     const san=prompt('San jaz')
+//     sandar.push(san)
+// }
+// console.log(sandar);
+
+
+// let num=0
+// for(; num<5; num++){
+//     console.log(num);
+//     if (num==2) break
+// }
+// console.log(Jumush toktodu num = ${num});
